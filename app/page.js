6ef37e8246b78cffc19a8aaac5d1c249fb634e57
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2023-12-12 22:46:51 +0400

    fixed bugs

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import DesignDetails from "@/Components/DesignDetails";
 import AboutUs from "@/components/AboutUsDetails";
 import OurTeam from "@/components/AboutUsDetails/Team";
 import HomeDetails from "@/components/HomeDetails";
 import Work from "@/components/HomeDetails/Work";
 import OurPartners from "@/components/HomeDetails/Partners";
 import Services from "@/components/ServicesDetails/OurServices";
+import DesignDetails from "@/components/DesignDetails";
 
 export default function Home() {
     return <>
